bc349dff35666aff3aea7d8a713a7d8fb0d29c13
Victor Manuel Ninahuanca Ayala <mninahuanca4@gmail.com>
2020-10-25 12:32:23 -0400

    validacion de formulario usando States

--- src/components/formulario.js
+++ src/components/formulario.js
@@ -1,25 +1,74 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const Formulario = () => {
+
+    // Crear State de Citas
+
+    const [cita, actualizarCita] = useState({
+        mascota: '',
+        propietario: '',
+        fecha: '',
+        hora: '',
+        sintomas: ''
+    })
+
+    const [error,actualizarError] = useState(false)
+
+    // Funcion que se ejecuta cada que el usuario escribe en un input
+
+    const actualizarState = e =>{
+        actualizarCita({
+            ...cita,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    const {mascota,propietario,fecha,hora,sintomas} = cita;
+
+    //Cuando el usuario presiona agregar cita
+
+    const submitCita = e => {
+        e.preventDefault();
+
+        // Validar
+        if (mascota.trim() === '' || propietario.trim() === '' || fecha.trim() === '' || hora.trim() === ''  || sintomas.trim() === ''  ) {
+            actualizarError(true);
+            return;
+        }
+
+
+        // Asignar un ID
+
+        // Crear la cita 
+
+        // Reiniciar el form
+    }
+
     return (
         <Fragment>
             <h2>Crear Cita</h2>
-
-            <form>
+            {error ? <p className="alerta-error">Todoslos campos son obligatorios</p> :null}
+            <form
+                onSubmit = {submitCita}
+            >
                 <label htmlFor="">Nombre Mascota</label>
                 <input
                     type="text"
                     name="mascota"
                     className="u-full-width"
                     placeholder="Nombre Mascota"
+                    onChange={actualizarState}
+                    value = {mascota}
                 />
 
                 <label htmlFor="">Nombre Dueño</label>
                 <input
                     type="text"
-                    name="dueño"
+                    name="propietario"
                     className="u-full-width"
                     placeholder="Nombre Dueño"
+                    onChange={actualizarState}
+                    value = {propietario}
                 />
 
                 <label htmlFor="">Fecha</label>
@@ -27,6 +76,8 @@ const Formulario = () => {
                     type="date"
                     name="fecha"
                     className="u-full-width"
+                    onChange={actualizarState}
+                    value = {fecha}
                 />
 
                 <label htmlFor="">Hora</label>
@@ -34,18 +85,22 @@ const Formulario = () => {
                     type="time"
                     name="hora"
                     className="u-full-width"
+                    onChange={actualizarState}
+                    value = {hora}
                 />
 
                 <label htmlFor="">Sintomas</label>
                 <textarea
                     type="time"
-                    name="hora"
+                    name="sintomas"
                     className="u-full-width"
+                    onChange={actualizarState}
+                    value = {sintomas}
                 ></textarea>
 
                 <button
-                    type = "submit"
-                    className ="u-full-width button-primary"
+                    type="submit"
+                    className="u-full-width button-primary"
                 >Agregar cita</button>
 
 
